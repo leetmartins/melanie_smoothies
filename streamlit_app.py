c0075b77191660e8c0a99f752cb3ffdd3d14b674
Letícia Martins <108501966+leetmartins@users.noreply.github.com>
2025-01-24 10:37:20 -0300

    Update streamlit_app.py
    Integrate de Smoothie API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title("Customize your Smoothie!:cup_with_straw:")
@@ -44,3 +45,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
